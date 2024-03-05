46e042b21be8ace166cb450aee5d9c9a221ca9fe
jsong-kentech <jsong@kentech.ac.kr>
2024-03-05 14:41:45 +0900

    22222
    222

--- OCV_inclass.m
+++ OCV_inclass.m
@@ -1,9 +1,10 @@
 clear
 clc; close all
 
-
-path_folder = 'G:\Shared drives\EE6211_2024\Data\FCC_(5)_OCV_C20';
-filename = 'HNE_FCC_06_OCV_027.txt';
+%% Load DATA
+path_folder = 'G:\Shared drives\EE6211_2024\Data\FCC_(5)_OCV_C100';
+filename = 'HNE_FCC_01_OCV_022.txt';
+savefilename = 'OCV_inclass.mat';
 
 data_now = readtable([path_folder filesep filename],'FileType','text','NumHeaderLines',14, ...
     'readVariableNames',0); % load the data
@@ -23,26 +24,42 @@ plot(data1.t2, data1.V); hold on
 yyaxis right
 plot(data1.t2, data1.step)
 
-%% TAKE OCV STEP (CHG)
-ocv_chg_data(:,1) = seconds(data1.t2(data1.step ==4));
-ocv_chg_data(:,2) = data1.I(data1.step ==4);
-ocv_chg_data(:,3) = data1.V(data1.step ==4);
+figure(2)
+plot(data1.t2,data1.I)
+
+
+%% TAKE OCV STEP (CHG) %% PARSING
+ocv_chg_data(:,1) = seconds(data1.t2(data1.I>0));
+ocv_chg_data(:,2) = data1.I(data1.I>0);
+ocv_chg_data(:,3) = data1.V(data1.I>0);
+
+ocv_dch_data(:,1) = seconds(data1.t2(data1.step == 6));
+ocv_dch_data(:,2) = data1.I(data1.step == 6);
+ocv_dch_data(:,3) = data1.V(data1.step == 6);
 
 figure(2)
-plot(ocv_chg_data(:,1),ocv_chg_data(:,3))
-
+plot(ocv_chg_data(:,1),ocv_chg_data(:,3)); hold on
+plot(ocv_dch_data(:,1),ocv_dch_data(:,3))
 
 %% SOC CALCULTAION
 
-ocv_chg_data(:,4) = cumtrapz(ocv_chg_data(:,1),ocv_chg_data(:,2)); % [A*sec]
-ocv_chg_data(:,5) = ocv_chg_data(:,4)/(ocv_chg_data(end,4));
+ocv_chg_data(:,4) = cumtrapz(ocv_chg_data(:,1),ocv_chg_data(:,2)); % Q [A*sec]
+ocv_chg_data(:,5) = ocv_chg_data(:,4)/(ocv_chg_data(end,4)); % SOC
+
+ocv_dch_data(:,4) = cumtrapz(ocv_dch_data(:,1),ocv_dch_data(:,2)); % Q[A*sec]
+ocv_dch_data(:,5) = 1-ocv_dch_data(:,4)/(ocv_dch_data(end,4));% SOC
 
 figure(3)
 plot(ocv_chg_data(:,5),ocv_chg_data(:,3))
+hold on
+plot(ocv_dch_data(:,5),ocv_dch_data(:,3))
+legend('chg','dch')
+
 
 
 %% SAVE OCV
 
-OCV = [ocv_chg_data(:,5), ocv_chg_data(:,3)]; %[SOC [1], OCV [V]]
+OCV.chg = [ocv_chg_data(:,5), ocv_chg_data(:,3)]; %[SOC [1], OCV [V]]
+OCV.dch = [ocv_dch_data(:,5), ocv_dch_data(:,3)];
 
-save OCV_inclass_C20.mat OCV
+save(savefilename,'OCV') 
